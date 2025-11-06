997beda46db294f5ca34ca51f6d52020ea13efd4
Ubuntu <ubuntu@ip-172-31-9-163.ap-south-1.compute.internal>
2025-11-06 13:18:01 +0000

    flask demo

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    name = None
+    if request.method == "POST":
+        name = request.form.get("name")
+    return render_template("index.html", name=name)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
